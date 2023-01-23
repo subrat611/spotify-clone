987ebb330fb133547ee7046e53e40dd5c513046a
subrat <subratj611@gmail.com>
2023-01-24 02:47:52 +0530

    minor changes

--- src/pages/RootPage/index.jsx
+++ src/pages/RootPage/index.jsx
@@ -6,7 +6,7 @@ function RootPage() {
   return (
     <div className={style.root_container}>
       <div className={style.logo_container}>
-        <img src="src/assets/icons/spotify.png" className={style.logo__img} />
+        <img src="../../src/assets/icons/spotify.png" className={style.logo__img} />
         <span className={style.logo__title}>Spotify</span>
       </div>
       <div className={style.btn_container}>
