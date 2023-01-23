bb1f6dcbfb4fce73cddb1e799e52a8c9f99a93f9
subrat <subratj611@gmail.com>
2023-01-24 02:42:23 +0530

    root page ui done

---
+++ src/pages/RootPage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import style from "./rootpage.module.css";
+
+function RootPage() {
+  return (
+    <div className={style.root_container}>
+      <div className={style.logo_container}>
+        <img src="src/assets/icons/spotify.png" className={style.logo__img} />
+        <span className={style.logo__title}>Spotify</span>
+      </div>
+      <div className={style.btn_container}>
+        <button className="btn btn--signup">SIGN UP</button>
+        <button className="btn btn--login">LOG IN</button>
+      </div>
+    </div>
+  );
+}
+
+export default RootPage;
