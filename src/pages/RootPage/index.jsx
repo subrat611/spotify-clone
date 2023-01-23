583a0b691da210cc6b853b889e34b137c2346f23
subrat <subratj611@gmail.com>
2023-01-24 02:53:53 +0530

    minor changes

--- src/pages/RootPage/index.jsx
+++ src/pages/RootPage/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import style from "./rootpage.module.css";
 
+import logo from "../../assets/icons/spotify.png";
+
 function RootPage() {
   return (
     <div className={style.root_container}>
       <div className={style.logo_container}>
-        <img src="/src/assets/icons/spotify.png" className={style.logo__img} />
+        <img src={logo} className={style.logo__img} />
         <span className={style.logo__title}>Spotify</span>
       </div>
       <div className={style.btn_container}>
