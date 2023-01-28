80b889a1b27ed774e1ef252b6c791b9620e0689d
subrat <subratj611@gmail.com>
2023-01-28 18:45:07 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/AuthPage/Login";
+import Signup from "./pages/AuthPage/Signup";
 import RootPage from "./pages/RootPage";
 
 function App() {
   return (
-    <div className="App">
-      <RootPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<RootPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
